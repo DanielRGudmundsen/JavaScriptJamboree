6521626d8c7595d5b443fec6548d7a137a53623a
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-05-19 22:04:40 -0700

    UI improvements - added chalky font by linking it in the html head and adding the neccesary js - added button.jpeg - continued working on styling and layout

--- quiz.js
+++ quiz.js
@@ -15,6 +15,16 @@ let score = 0;
 
 document.getElementById('start-quiz').addEventListener('click', startQuiz);
 
+// Typekit font loader
+(function(d) {
+    var config = {
+      kitId: 'luq0eik',
+      scriptTimeout: 3000,
+      async: false
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document);
+
 function startQuiz() {
     console.log("Quiz Started!");
     // Hide start button
